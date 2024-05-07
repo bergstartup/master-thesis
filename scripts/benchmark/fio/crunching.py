00e83f92d45a888407175deceb63f43d751e6cb6
Sudarsan Sivakumar <appsby12@gmail.com>
2024-05-07 20:01:02 +0000

    New scripts and iouring config performance

--- scripts/benchmark/fio/crunching.py
+++ scripts/benchmark/fio/crunching.py
@@ -45,17 +45,20 @@ def get_experiment(node, dt, wt, qd, np):
 
 #Crunch all observations
 all_observations = {}
-experiments = list_all_experiments('local')
+experiments = list_all_experiments('remote_polling')
 for exp in experiments:
-    print(exp['NAME'])
-    with open(observation_dir+exp['NAME'],'r') as f:
-        data = json.load(f)
-        obs = data['jobs'][0][exp['TYPE']]
-        obs_dict = {}
-        obs_dict['latency'] = obs['clat_ns']
-        obs_dict['iops'] = {'min':obs['iops_min'],'max':obs['iops_max'],'mean':obs['iops_mean'],'stddev':obs['iops_stddev'],'N':obs['iops_samples']}
-        obs_dict['bw'] = {'min':obs['bw_min'],'max':obs['bw_max'],'mean':obs['bw_mean'],'stddev':obs['bw_dev'],'N':obs['bw_samples']}
-        all_observations[exp['NAME']] = obs_dict
+    try:
+        with open(observation_dir+exp['NAME'],'r') as f:
+            print(exp['NAME'])
+            data = json.load(f)
+            obs = data['jobs'][0][exp['TYPE']]
+            obs_dict = {}
+            obs_dict['latency'] = obs['clat_ns']
+            obs_dict['iops'] = {'min':obs['iops_min'],'max':obs['iops_max'],'mean':obs['iops_mean'],'stddev':obs['iops_stddev'],'N':obs['iops_samples']}
+            obs_dict['bw'] = {'min':obs['bw_min'],'max':obs['bw_max'],'mean':obs['bw_mean'],'stddev':obs['bw_dev'],'N':obs['bw_samples']}
+            all_observations[exp['NAME']] = obs_dict
+    except:
+        pass
 
 
 
