d26528dc88d9dc4b7dac2ee8723ecd01ff217fd1
Sudarsan Sivakumar <appsby12@gmail.com>
2024-07-13 17:25:23 +0000

    Change to target setup script

--- scripts/setup/nvmeof_tcp_target_setup.sh
+++ scripts/setup/nvmeof_tcp_target_setup.sh
@@ -2,26 +2,30 @@
 #!/bin/bash
 
 if [ "-h" == "$1" ]; then
-	echo "Requires 4 cmd line args: count device(/dev/nvme?n?) ip port"	
+	echo "Requires 4 cmd line args: device(/dev/nvme?n?) count"	
 	exit 1
 fi
 
 sudo modprobe nvmet
 sudo modprobe nvmet-tcp
+
+
+sudo mkdir /sys/kernel/config/nvmet/ports/1
+cd /sys/kernel/config/nvmet/ports/1
+echo 172.16.137.2 | sudo tee -a addr_traddr > /dev/null
+echo tcp | sudo tee -a addr_trtype > /dev/null
+echo 4420 | sudo tee -a addr_trsvcid > /dev/null
+echo ipv4 | sudo tee -a addr_adrfam > /dev/null
+
 cd /sys/kernel/config/nvmet/subsystems
-sudo mkdir nvme-test-target
+sudo mkdir thesis.dev$2
 cd nvme-test-target/
 echo 1 | sudo tee -a attr_allow_any_host > /dev/null
-sudo mkdir namespaces/$1
-cd namespaces/$1
+sudo mkdir namespaces/1
+cd namespaces/1
+echo -n $1 |sudo tee -a device_path > /dev/null
+echo 1 | sudo tee -a enable > /dev/null
 
-echo -n $2 |sudo tee -a device_path > /dev/null
-echo 1|sudo tee -a enable > /dev/null
-sudo mkdir /sys/kernel/config/nvmet/ports/$1
-cd /sys/kernel/config/nvmet/ports/$1
-echo $3 |sudo tee -a addr_traddr > /dev/null
-echo tcp|sudo tee -a addr_trtype > /dev/null
-echo $4|sudo tee -a addr_trsvcid > /dev/null
-echo ipv4 |sudo tee -a addr_adrfam > /dev/null
-sudo ln -s /sys/kernel/config/nvmet/subsystems/nvme-test-target/ /sys/kernel/config/nvmet/ports/$1/subsystems/nvme-test-target
-dmesg | grep $3
+
+sudo ln -s /sys/kernel/config/nvmet/subsystems/nvme-test-target/ /sys/kernel/config/nvmet/ports/1/subsystems/thesis.dev$2
+sudo dmesg | grep 4420
